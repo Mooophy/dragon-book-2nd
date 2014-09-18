a2ea4e0a6d22d0774f3ff2789717c4079426e050
mooophy <MoophyAlan@gmail.com>
2014-09-19 09:53:39 +1200

    update : use lambda + do while instead. 	modified:   ex2_4_1a.cpp

--- ch02/ex2_4_1a.cpp
+++ ch02/ex2_4_1a.cpp
@@ -8,30 +8,9 @@
 //!
 #include <iostream>
 #include <string>
+#include <functional>
 
 namespace dragon{ namespace ch2{
-
-/**
- * @brief parse
- * @param curr      i.e. lookahead
- * @param ret       bool to store whether the syntax legal
- *
- * perform the real work
- */
-template<typename Iter>
-void parse(Iter& curr, bool& ret)
-{
-    if(*curr == 'a')
-        ++curr;
-    else if(*curr == '+'   ||  *curr == '-')
-    {
-        ++curr;
-        parse(curr, ret);
-        parse(curr, ret);
-    }
-    else
-        ret = false;
-}
 
 /**
  * @brief parse_241a
@@ -42,17 +21,34 @@ void parse(Iter& curr, bool& ret)
 template<typename Iter>
 inline bool parse_241a(Iter first, Iter last)
 {
-    bool ret = true;
-    Iter curr= first;
-    while(1)
+    bool is_legal = true;
+
+    //! define a lambda for real work
+    std::function<Iter(Iter)> s = [&](Iter curr)
     {
-        parse(curr, ret);
+        if(curr == last)
+        {
+            is_legal = false;
+            return last;
+        }
 
-        if(curr == last)    break;
-        else                ++curr;
-    }
+        if(*curr == 'a')
+            return curr + 1;
+        else if(*curr == '+'   ||  *curr == '-')
+            return s(s(curr + 1));
+        else
+        {
+            is_legal = false;
+            return last;
+        }
+    };
 
-    return ret;
+    //! call the lambda
+    Iter curr = first;
+    do  curr = s(curr);
+    while(curr++ != last);
+
+    return is_legal;
 }
 }}//namespace
 
@@ -60,12 +56,14 @@ inline bool parse_241a(Iter first, Iter last)
 int main()
 {
     std::cout << "enter statements, pls follow : S -> '+' S S | '-' S S | 'a'\n";
+
     for(std::string buff; std::cin >> buff; /* */)
     {
         bool is_legal = dragon::ch2::parse_241a(buff.begin(),buff.end());
         std::cout << "@parser ex2.4.1.a --> "
                   << (is_legal?     "legal\n"     :   "syntax error\n");
     }
+
     return 0;
 }
 
